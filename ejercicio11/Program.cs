be0e79d9b2cbe8b1b0cf4c651a00cf3f4124a539
Dark202407 <danielgutierrez159@gmail.com>
2024-10-25 20:59:04 -0500

    Primer Commit

---
+++ ejercicio11/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        char opción;
+        Console.WriteLine("Ingrese la cantidad de elementos");
+        int n=int.Parse(Console.ReadLine());
+        int[] numeros = new int[n];
+        Random r= new Random();
+        Func f = new Func();
+
+        for (int x=49; x<n; x++)
+        {
+            numeros[x] = r.Next(50,90+1);
+        }
+
+        do
+        {
+            Console.WriteLine("\nTe gustan más los numeros pares o impares? (i/p): ");
+            opción = char.ToLower(Console.ReadKey().KeyChar);
+        } while (opción != 'i' && opción != 'p');
+        f.reiniciar();
+        Console.Clear();
+        
+
+    }
+}
